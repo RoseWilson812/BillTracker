e6806e0fee31e3dc052e8b92f137534aa30ce8da
Rose Wilson <rosewilson812@yahoo.com>
2021-07-01 23:08:41 -0500

    Added the ability to delete a bill

--- BillTracker/ViewModels/DeleteBillViewModel.cs
+++ BillTracker/ViewModels/DeleteBillViewModel.cs
@@ -1,70 +1,78 @@
 ﻿using BillTracker.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore.Internal;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace BillTracker.ViewModels
 {
-    public class AddBillViewModel
+    public class DeleteBillViewModel
     {
+        public int Id { get; set; }
         [DataType(DataType.Date)]
-        [DisplayFormat(DataFormatString = "{0:mm/dd/yyyy}", ApplyFormatInEditMode = true)]
-        [Required(ErrorMessage = "A valid date must be entered!")]
+              
         public DateTime DueDate { get; set; }
 
         [DataType(DataType.Date)]
-        [DisplayFormat(DataFormatString = "{0:mm/dd/yyyy}", ApplyFormatInEditMode = true, ConvertEmptyStringToNull = false)]
-
         public DateTime? PaidDate { get; set; }
 
-        [Required(ErrorMessage = "Payee is required!")]
-        [MaxLength(40)]
         public string Payee { get; set; }
 
+     
         [DataType(DataType.Currency)]
-        [Range(.01, 1000000000, ErrorMessage = "Amount must be greater than 0.00")]
-        [Required(ErrorMessage = "Amount is required!")]
         public decimal Amount { get; set; }
 
         [BindProperty]
         public int CategoryId { get; set; }
 
-        [MaxLength(40)]
+        public string CategoryName { get; set; }
         public string Memo { get; set; }
-
-        [Required(ErrorMessage = "Tax Deductible must be Y or N")]
+                
         public char TaxDeductible { get; set; }
         public List<SelectListItem> PossibleCategories { get; set; }
         public static List<BillCategory> SaveCategories { get; set; }
-        //public int CategoryId { get; set; }
+
         public BillCategory Categorys { get; set; }
         public static string[] SelectedCategory { get; set; }
- //       public static int SaveMemberId {get; set;}
+
         public static Member Member { get; set; }
         public List<Bill> SaveBills { get; set; }
 
 
-        public AddBillViewModel(List<BillCategory> categories, Member memberId) //DateTime formattedDate)
+        public DeleteBillViewModel(int id, DateTime dueDate, DateTime? paidDate, string payee,
+            decimal amount, int categoryId, string memo, char taxDeductible, List<BillCategory> categories,
+            Member member)
         {
-        //    DueDate = formattedDate;
-        //    PaidDate = formattedDate;
-  //          Memo = "";
-            TaxDeductible = 'N';
-            Member = memberId;
- //           PossibleCategories = new List<SelectListItem>();
-            SaveCategories = new List<BillCategory>();
-            SaveCategories = categories.GetRange(0, categories.Count);
-            CreateDropdown();
- 
-        }
+            Id = id;
+            DueDate = dueDate;
+            PaidDate = paidDate;
+            Payee = payee;
+            Amount = amount;
+            CategoryId = categoryId;
+            Memo = memo;
+            TaxDeductible = taxDeductible;
 
-        public AddBillViewModel()
+            Member = member;
+            //SaveCategories = new List<BillCategory>();
+            SaveCategories = categories.GetRange(0, categories.Count);
+            
+            foreach (var category in categories)
+            {
+                if (categoryId == category.Id)
+                {
+                    CategoryName = category.CategoryName;
+                    break;
+                }
+            }
+        }
+        public DeleteBillViewModel()
         {
-        }          
+        }
         public void CreateDropdown()
         {
             PossibleCategories = new List<SelectListItem>();
@@ -79,6 +87,6 @@ namespace BillTracker.ViewModels
                 ); ;
             }
         }
-
     }
+
 }
